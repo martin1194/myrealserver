f8eb38bf8ff7119abff9034842bb6a4a3d2004e5
martin1194 <martinc1194@gmail.com>
2019-12-01 00:33:39 +0900

    1 file modified

--- src/app/bearing.service.ts
+++ src/app/bearing.service.ts
@@ -1,28 +1,37 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams
+} from "@angular/common/http";
 
-import { Observable, throwError } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { Observable, throwError } from "rxjs";
+import { map, catchError } from "rxjs/operators";
 
-import { bearing } from './bearing';
+import { bearing } from "./bearing";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
-export class BearingService {baseUrl = 'http://treeeee-bear.com/back-end';
-bearings: bearing[];
-                
-constructor(private http: HttpClient) { }
-                
-getAll(): Observable<bearing[]> {
-  return this.http.get(`${this.baseUrl}/list`).pipe(
-    map((res) => {
-      this.bearings = res['data'];
-      return this.bearings;
-  }),
-  catchError(this.handleError));
+export class BearingService {
+  baseUrl = "http://treeeee-bear.com/back-end";
+  bearings: bearing[];
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<bearing[]> {
+    return this.http.get(`${this.baseUrl}/list`).pipe(
+      map(res => {
+        this.bearings = res["data"];
+        return this.bearings;
+      }),
+      catchError(this.handleError)
+    );
+  }
+  private handleError(error: HttpErrorResponse) {
+    console.log(error);
+
+    // return an observable with a user friendly message
+    return throwError("Error! something went wrong.");
+  }
 }
-}
-
-
-
