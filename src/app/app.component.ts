8158ad908d35c2977d6c1a4250849417613e318c
martin1194 <martinc1194@gmail.com>
2019-12-01 00:43:17 +0900

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { bearing } from './bearing';
+import { BearingService } from './bearing.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular';
+  cars: bearing[];
+  error = '';
+  success = '';
+        
+  constructor(private carService: BearingService) {
+  }
+        
+  ngOnInit() {
+    this.getCars();
+  }
+        
+  getCars(): void {
+  }
 }
