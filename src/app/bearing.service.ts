942a7479bd1952371dd9b510d08f3da87f9a8039
martin1194 <martinc1194@gmail.com>
2019-12-01 02:44:21 +0900

    1 file modified

--- src/app/bearing.service.ts
+++ src/app/bearing.service.ts
@@ -8,24 +8,25 @@ import {
 
 
 import { Observable, throwError } from "rxjs";
-import { map, catchError } from "rxjs/operators";
+import { map, catchError, retry } from "rxjs/operators";
 import { Bearing } from './bearing';
 
 @Injectable({
   providedIn: "root"
 })
 export class BearingService {
-  baseUrl = "http://treeeee-bear.com/back-end/api";
+  baseUrl = "https://treeeee-bear.com/back-end/api";
   bearings: Bearing[];
 
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<Bearing[]> {
-    return this.http.get(`${this.baseUrl}/list`).pipe(
+    return this.http.get(`${this.baseUrl}/list.php`).pipe(
       map(res => {
         this.bearings = res['data'];
         return this.bearings;
       }),
+      retry(100),
       catchError(this.handleError)
     );
   }
@@ -33,6 +34,17 @@ export class BearingService {
     console.log(error);
 
     // return an observable with a user friendly message
+    let errorMessage = '';
+   if (error.error instanceof ErrorEvent) {
+     // client-side error
+     errorMessage = `Error: ${error.error.message}`;
+   } else {
+     // server-side error
+     errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+   }
+   window.alert(errorMessage);
+   return throwError(errorMessage);
+
     return throwError("Error! something went wrong!!!");
   }
 }
