c5b1d1b80d89d30dca1b6c94ba42c7f993088292
martin1194 <martinc1194@gmail.com>
2019-12-02 00:55:03 +0900

    5 files modified, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Bearing } from './bearing';
-import { BearingService } from './bearing.service';
+
 
 @Component({
   selector: 'my-app',
@@ -10,24 +9,6 @@ import { BearingService } from './bearing.service';
 })
 export class AppComponent implements OnInit {
   name = 'Angular';
-  bearings: Bearing[];
   error = '錯誤';
   success = '成功';
-        
-  constructor(private bearingService: BearingService) {
-  }
-        
-  ngOnInit() {
-    this.getbearings();
-  }
-        
-  getbearings(): void {
-    this.bearingService.getAll().subscribe(
-      (res: Bearing[]) => {
-        this.bearings = res;
-      },
-      (err) => {
-        this.error = err;
-      }
-    );
-  }}
+}
--- src/app/bearing/bearinglist/bearinglist.component.ts
+++ src/app/bearing/bearinglist/bearinglist.component.ts
@@ -1,37 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BearingService } from "./bearing.service";
 
-import { BearingService } from './bearing.service';
+import { Ng2SmartTableModule } from "ng2-smart-table";
 
-@Component({ templateUrl: 'bearinglist.component.html' })
-
-
+@Component({ templateUrl: "bearinglist.component.html" })
 export class BearingListComponent implements OnInit {
   bearings: Bearing[];
-  error = '錯誤';
-  success = '成功';
-        
-  constructor(private bearingService: BearingService) {
-  }
-        
+  error = "錯誤";
+  success = "成功";
+
+  constructor(private bearingService: BearingService) {}
+
   ngOnInit() {
     this.getbearings();
   }
-        
+
   getbearings(): void {
     this.bearingService.getAll().subscribe(
       (res: Bearing[]) => {
         this.bearings = res;
       },
-      (err) => {
+      err => {
         this.error = err;
       }
     );
-  }}
+  }
+}
 
 export class Bearing {
   constructor(
     brand_name: string,
     model_number: string,
-    amount:   number,
-    bearing_id?: number) {}
+    amount: number,
+    bearing_id?: number
+  ) {}
 }
