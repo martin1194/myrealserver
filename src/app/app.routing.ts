c06f69c5cf56cecb1639400d4cec04ba0dc32252
martin1194 <martinc1194@gmail.com>
2019-12-01 23:52:39 +0900

    2 files modified

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
+import { BearingListComponent } from './bearing/bearinglist';
+
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'bearinglist', component: BearingListComponent },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
