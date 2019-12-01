cf8ce9f61c679d32e04e6982fd140ff2d522f03a
martin1194 <martinc1194@gmail.com>
2019-12-01 11:56:45 +0800

    1 file modified

--- src/app/bearing.service.ts
+++ src/app/bearing.service.ts
@@ -15,7 +15,7 @@ import { Bearing } from './bearing';
   providedIn: "root"
 })
 export class BearingService {
-  baseUrl = "http://treeeee-bear.com/back-end/api";
+  baseUrl = "https://treeeee-bear.com/back-end/api";
   bearings: Bearing[];
 
   constructor(private http: HttpClient) {}
