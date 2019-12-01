19ee36df2e6bf148e3137c3720cafe24d58188fb
martin1194 <martinc1194@gmail.com>
2019-12-01 22:08:59 +0900

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { appRoutingModule } from './app.routing';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
 import { HelloComponent } from './hello.component';
 
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    HttpClientModule, 
+    appRoutingModule 
+  ],
   declarations: [ 
     AppComponent, 
     HelloComponent, 
     HomeComponent,
+    RegisterComponent,
     LoginComponent, 
   ],
   bootstrap:    [ AppComponent ]
