e8e77e7b0f61b1131accd30aeb265fe1a76911c9
martin1194 <martinc1194@gmail.com>
2019-12-02 00:41:12 +0900

    1 file modified, 1 added

--- src/app/bearing/bearinglist/bearinglist.component.ts
+++ src/app/bearing/bearinglist/bearinglist.component.ts
@@ -1,4 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { BearingService } from './bearing.service';
 
 @Component({ templateUrl: 'bearinglist.component.html' })
-export class BearingListComponent {}
+
+
+export class BearingListComponent implements OnInit {
+  bearings: Bearing[];
+  error = '錯誤';
+  success = '成功';
+        
+  constructor(private bearingService: BearingService) {
+  }
+        
+  ngOnInit() {
+    this.getbearings();
+  }
+        
+  getbearings(): void {
+    this.bearingService.getAll().subscribe(
+      (res: Bearing[]) => {
+        this.bearings = res;
+      },
+      (err) => {
+        this.error = err;
+      }
+    );
+  }}
+
+export class Bearing {
+  constructor(
+    brand_name: string,
+    model_number: string,
+    amount:   number,
+    bearing_id?: number) {}
+}
