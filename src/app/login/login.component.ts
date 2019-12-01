dbd64976c0ff02a9efb38959c47da2043c434575
martin1194 <martinc1194@gmail.com>
2019-12-02 00:17:52 +0900

    2 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
 
-@Component({ templateUrl: 'login.component.html' })
+@Component({ 
+  templateUrl: 'login.component.html' ,
+  selector: 'my-app1',})
 export class LoginComponent {}
